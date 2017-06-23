b463dec5151f16510069ca180d8ff1fa9effc28a
2868397063@qq.com <2868397063@qq.com>
2017-06-23 10:49:12 +0800

    307跳转

---
+++ http/05.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kobe
+ * Date: 2017/6/23
+ * Time: 上午10:41
+ */
+
+ header('location:06.php',true,307);
---
+++ http/06.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kobe
+ * Date: 2017/6/23
+ * Time: 上午10:42
+ */
+  print_r($_POST);
